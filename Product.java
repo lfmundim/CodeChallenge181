d365b491890ac924726f80ecf043fad615636e3f
PedroPrisxzma <pep445@hotmail.com>
2018-05-07 10:52:57 -0300

    Add files via upload

--- Product.java
+++ Product.java
@@ -5,29 +5,30 @@ public class Product implements IProduct{
   private String Name;
   private float Cost;
   private ProductType Type;
+
   public Product(){
     
   }
   public ProductType GetType(){
-
+    return this.Type
   }
   public void SetType(){
 
   }
   public int GetId(){
-
+    return this.Id
   }
   public void SetId(){
 
   }
   public String GetName(){
-
+    return this.Name
   }
   public void SetName(){
 
   }
   public float GetCost(){
-
+    return this.Cost
   }
   public void SetCost(){
 
